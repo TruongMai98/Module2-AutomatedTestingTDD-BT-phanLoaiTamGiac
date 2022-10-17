f16e6eee7891431c197d346395189b46157b5096
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 01:26:01 +0700

    pass test tam giac deu

--- src/TriangleClassifier.java
+++ src/TriangleClassifier.java
@@ -1,5 +1,8 @@
 public class TriangleClassifier {
     public static String getTypeOfTriangle(int a, int b, int c) {
+        if (a == b && b == c && a == c) {
+            return "tam giac deu";
+        }
         return null;
     }
 }
