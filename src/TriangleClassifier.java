2d4673a232244d23f2b8f0c32c0052c514a3559e
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 01:33:22 +0700

    refactor test tam giac deu ver2

--- src/TriangleClassifier.java
+++ src/TriangleClassifier.java
@@ -1,7 +1,9 @@
 public class TriangleClassifier {
     public static String getTypeOfTriangle(int side1, int side2, int side3) {
-        boolean side1EqualSide2EqualSide3 = side1 == side2 && side2 == side3 && side1 == side3;
-        if (side1EqualSide2EqualSide3) {
+        boolean side1EqualSide2 = side1 == side2;
+        boolean side2EqualSide3 = side2 == side3;
+        boolean side3EqualSide1 = side1 == side3;
+        if (side1EqualSide2 && side2EqualSide3 && side3EqualSide1) {
             return "tam giac deu";
         }
         return null;
