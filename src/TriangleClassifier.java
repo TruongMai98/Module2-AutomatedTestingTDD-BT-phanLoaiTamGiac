0d51c963d56f0f98c0e53f57db4ccb74c0741b9c
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 02:21:55 +0700

    refactor test khong phai tam giac

--- src/TriangleClassifier.java
+++ src/TriangleClassifier.java
@@ -7,10 +7,16 @@ public class TriangleClassifier {
             return "tam giac deu";
         } else if (side1EqualSide2 || side2EqualSide3 || side3EqualSide1) {
             return "tam giac can";
-        } else if (Math.abs(side1 - side2) < side3 && Math.abs(side1 - side2) < (side1 + side2) && side3 < (side1 + side2)) {
+        } else if (isTriangle(side1, side2, side3)) {
             return "tam giac thuong";
         } else {
             return "khong phai tam giac";
         }
+    }
+
+    private static boolean isTriangle(int side1, int side2, int side3) {
+        return Math.abs(side1 - side2) < side3 &&
+                Math.abs(side1 - side2) < (side1 + side2) &&
+                side3 < (side1 + side2);
     }
 }
