09565e047abba7613d0bb671eb4d512685c292ce
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 01:35:02 +0700

    add test tam giac can

--- src/TriangleClassifierTest.java
+++ src/TriangleClassifierTest.java
@@ -15,5 +15,15 @@ class TriangleClassifierTest {
         String excepted = "tam giac deu";
         String result = TriangleClassifier.getTypeOfTriangle(side1, side2, side3);
         assertEquals(excepted, result);
+    } @Test
+    @DisplayName("tam giac can")
+    void getTypeOfTriangle1() {
+        int side1 = 2;
+        int side2 = 2;
+        int side3 = 3;
+
+        String excepted = "tam giac can";
+        String result = TriangleClassifier.getTypeOfTriangle(side1, side2, side3);
+        assertEquals(excepted, result);
     }
 }
